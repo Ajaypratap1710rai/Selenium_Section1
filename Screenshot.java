e85642e9d1d06f83a9d57b9403ec5d2f44ff6b53
Ajaypratap1710rai <66253886+Ajaypratap1710rai@users.noreply.github.com>
2023-02-10 20:52:25 +0530

    Add files via upload

---
+++ Screenshot.java
@@ -0,0 +1,25 @@
+package section1_selenium.java;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshot {
+   public static void main(String[] args) throws IOException {
+	      System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	   	  WebDriver driver=new ChromeDriver();
+	   	  driver.manage().window().maximize();
+	   	  driver.get("https://www.amazon.in/");
+	   	  TakesScreenshot ts=(TakesScreenshot)driver;
+	   	  File src=ts.getScreenshotAs(OutputType.FILE);
+	   	  File trg=new File("./screenshot/google.png");
+	   	  FileUtils.copyFile(src, trg);
+	   	  
+	   	  
+ }
+}
